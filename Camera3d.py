ee50d3d40d0ed86ea453076b5eb6408ae2e78599
Robin Wang <westman021@qq.com>
2023-02-16 22:51:30 +0800

    Create Camera3d.py

--- Camera3d.py
+++ Camera3d.py
@@ -13,15 +13,13 @@ img_cartoon = pipeline(Tasks.image_portrait_stylization,
                       model='damo/cv_unet_person-image-cartoon_compound-models')
 
 cap = cv2.VideoCapture(0) # Capture video from camera
+
 # Get the width and height of frame
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)  + 0.5)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  + 0.5)
-# Create named window with normal flags
-
-
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
 # out = cv2.VideoWriter('output3d.mp4', fourcc, 10.0, (width, height))
 global i
 i = 0 
@@ -30,26 +28,21 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         
-         
-        imagename=str(i)+ "frame.png"
-        path="/Users/robin/PycharmProjects/AI_cartoon/tmp/" + imagename
-        
         result = img_cartoon(frame)
 
         frame = np.uint8(result[OutputKeys.OUTPUT_IMG])
+        # Draw the Chinese characters on the image using cv2.putText()
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = 'A-Big-SB'
+        cv2.putText(frame, text, (200, 300), font, 2, (0, 0, 255), 2, cv2.LINE_AA)
 
-        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-
-
-        cv2.imshow('frame',frame)
-
+        cv2.namedWindow(text, cv2.WINDOW_NORMAL)
+        cv2.imshow(text,frame)
         # Set window property to always on top
-        cv2.setWindowProperty('frame', cv2.WND_PROP_TOPMOST, 1)
+        cv2.setWindowProperty(text, cv2.WND_PROP_TOPMOST, 2)
         # Change window size
-        cv2.resizeWindow("frame",400,250)
+        cv2.resizeWindow(text,500,280)
 
-        # Set window property to always on top
-        cv2.setWindowProperty('frame', cv2.WND_PROP_TOPMOST, 1)
         if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
             break
     else:
